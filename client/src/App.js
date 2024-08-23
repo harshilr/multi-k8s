1c5a697c47a34a083686c5fdb7c1d59ad36481e7
harshil ramswaroop <hramswaroop@gmail.com>
2024-08-22 22:10:22 -0400

    updated header UI

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator version 2.0
+            Fib calculator version kubernetes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
